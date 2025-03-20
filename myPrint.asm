bbffe59818ad20ed8d170b62531eb3ca963047ee
Carvalanga <stratilatov.ea@phystech.edu>
2025-03-20 21:19:41 +0300

    realese

--- myPrint.asm
+++ myPrint.asm
@@ -10,24 +10,41 @@
 ;----------------------------------------------------------------------------
 ;								  CONSTANTS
 ;----------------------------------------------------------------------------
-BUF_SIZE 	  equ 4d
+BUF_SIZE       equ 32d
+ONES_IN_MASK16 equ 4
+ONES_IN_MASK8  equ 3
+ONES_IN_MASK2  equ 1
 
+
+MASK16 	  equ 1111b
+MASK8     equ 111b
+MASK2	  equ 1b
+SIGN_MASK equ 1 << 31
+
+
+BASE10 equ 10
 
 ;----------------------------------------------------------------------------
 ;=====================================MACRO==================================
 ;----------------------------------------------------------------------------
-%macro putchar 0
+%macro putChar 0
 		cmp rdi, BUFFER + BUF_SIZE
 		jne .no_overflow
 
 		call printBuffer
 
 	.no_overflow:
-		inc rdx
+		inc rbx
 		stosb
 
 %endmacro
 
+%macro getArgument 0
+
+		mov qword rax, [rbp]
+		add rbp, 8
+
+%endmacro
 
 ;----------------------------------------------------------------------------
 ;									CODE
@@ -45,286 +62,357 @@ section 	.text
 ;Entry:
 ;		rdi = current position of buffer string address
 ;		rsi = current position of format string address
-;		rdx = cnt of printed symbol
+;
 ;
 ;Exit:
-;		rax cnt of printed symbol
+;		rbx = count of printed symbols
 ;
 ;Destr:
 ;		rcx; r8(ret addres);
-;----------------------------------------------------------------------------
+;----------------------------------------------------------------------------------------------------------------------------------------------
 myPrint:
-		cld
+;---------------------PREPARING-FOR-THE-FUNCTION-WORK------------------------
 
-		pop rax
+		cld 										;clear direction flag
 
-		push r9 					;|push argumets in stack
-		push r8 					;|
-		push rcx					;|
-		push rdx					;|
-		push rsi 					;|
+		pop rax										;pop return address
 
-		push rbp					;set_new base point
-		mov  rbp, rsp
-		add  rbp, 8
+		push r9 									;|TRAMPLINE
+		push r8 									;|push argumets in stack
+		push rcx									;|
+		push rdx									;|
+		push rsi 									;|
 
-		push rbx
-		push r12
+		push rbp									;|set bp to the first argument
+		mov  rbp, rsp								;|
+		add  rbp, 8	 								;|
 
-		mov  r8, rax
-		xor  rdx, rdx						;writen sybmols
+		push r12 									;|save registers which don't have to be modified
+		push r13 									;|
+		push rbx 									;|
 
-		mov  rsi, BUFFER
-		xchg rsi, rdi
+		xor rbx, rbx								;|prepare registers for work
+		mov r8, rax 								;|
+		mov rsi, rdi  		 						;|
+		mov rdi, BUFFER								;|
+		xor rax, rax								;|
+
+;----------------------------------MAIN-BODY---------------------------------
 
 	bufferisation:
-		cmp byte [rsi], 0
-		je end_of_bufferisation
+		mov byte al, [rsi]							;get symbol
 
-		cmp byte [rsi], '%'
-		jne no_percent
+		cmp al, 0									;|if al == 0 (end of string)
+		je end_of_bufferisation						;|						goto end
 
-		call percentHandler
-		jmp bufferisation
+		cmp al, '%' 								;|if al != '%'
+		jne no_percent	 							;|  		continue
 
-	no_percent:				;to a new function
-		lodsb
-		putchar
-		jmp bufferisation
+		call percentHandler							;|
+		jmp  bufferisation							;|
+
+	no_percent:
+		inc rsi 									;rsi++
+		putChar 									;see MACRO
+		jmp bufferisation 							;continue bufferisation
+
+;------------------------PREPARING-TO-EXIT-FROM-FUNCTION---------------------
 
 	end_of_bufferisation:
-		push rdx
-		mov rdx, rdi
-		sub rdx, BUF_SIZE
-		call printBuffer
-		pop rax
 
-		pop r12
-		pop rbx
-		pop rbp
-		add rsp, 40
+		call printBuffer 							;last buffer print
 
-		push r8 			;arexit ???
+		mov rax, rbx 								;set return value to rax
+
+		pop rbx 									;|recover regs values
+		pop r13 									;|
+		pop r12										;|
+		pop rbp 									;|
+
+		add rsp, 40 								;clear stack frame
+
+		push r8 									;push return address
 		ret
 
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
-;----------------------------------------------------------------------------
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
 ;printBuffer:
+;		prints buffer from address of buffer start to address which
+;		contains rdi
 ;
-;Entry:
-;	rax = size to print
-;
-;----------------------------------------------------------------------------
+;Destr:	rcx, r11, rdx
+;----------------------------------------------------------------------------------------------------------------------------------------------
 printBuffer:
-		push rax
-		push rdx
-		push rsi 				;|
-		push rcx
+		push rax									;|save regs
+		push rsi 									;|
 
-		mov rdx, rdi
-		sub rdx, BUFFER
+		mov rdx, rdi 								;|set size to print
+		sub rdx, BUFFER 							;|
 
-		mov rax, 1				;|print string
-		mov rdi, 1				;|to stdout
-		mov rsi, BUFFER			;|started in BUFFER
+		mov rax, 1									;|print string
+		mov rdi, 1									;|to stdout
+		mov rsi, BUFFER								;|started in BUFFER
 
-		syscall					;|
+		syscall										;call print
 
-		mov rdi, BUFFER			;reset rdi to start of BUFFER
+		mov rdi, BUFFER								;reset rdi to start of BUFFER
 
-		pop rcx
-		pop rsi 				;|recover regs
-		pop rdx
+		pop rsi 									;|recover regs
 		pop rax
 		ret
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
 
-;----------------------------------------------------------------------------
+;----------------------------------------------------------------------------------------------------------------------------------------------
 ;percentHandler:
-;		%c = 99, %s = 115, %o = 111, %x = 120, %% = 37, %d = 100, %b = 98,
+;		checks symbol after percent and do something in accordance
+;		with jump table
 ;
 ;Entry:
-;		rdi = data
-;		rsi = buffer
+;		rsi = percent address
+;		rdi = buffer
 ;
 ;Destr:
-;
-;----------------------------------------------------------------------------
+;		rax
+;----------------------------------------------------------------------------------------------------------------------------------------------
 percentHandler:
-		xor rcx, rcx
-		inc rsi
-		mov byte cl, [rsi]
-		sub cl, '%'
-		cmp cl, 0
-		jb SWITCH_END
-		cmp cl, 'z' - '%'
-		ja SWITCH_END
+		inc rsi 									;skip %
+		mov byte al, [rsi] 							;al = current sybmol after percent
 
-		jmp [JMP_TABLE + rcx * 8]
+		sub al, '%' 								;|calculations for jump table
+		cmp al, 0 									;|check extreme cases
+		jb SWITCH_END	 							;|
+		cmp al, 'z' - '%'							;|
+		ja SWITCH_END	 							;|
+
+		jmp [JMP_TABLE + rax * 8] 					;JUMP!!! (to case)
 
 	SWITCH_END:
-		inc rsi
+		inc rsi 									;get next symbol
 		ret
+
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;															JMP TABLE AND CASES
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
 JMP_TABLE:
-	dq CASE_PERCENT
+					      dq CASE_PERCENT
 	times ('b' - '%' - 1) dq SWITCH_END
-	dq CASE_b
-	dq CASE_c
-	dq CASE_d
+						  dq CASE_b
+						  dq CASE_c
+						  dq CASE_d
 	times ('o' - 'd' - 1) dq SWITCH_END
-	dq CASE_o
+						  dq CASE_o
 	times ('s' - 'o' - 1) dq SWITCH_END
-	dq CASE_s
-	times ('x' - 's' - 1) dq SWITCH_END
-	dq CASE_x
+						  dq CASE_s
+						  dq SWITCH_END
+						  dq CASE_u
+	times ('x' - 'u' - 1) dq SWITCH_END
+						  dq CASE_x
 	times ('z' - 'x' - 1) dq SWITCH_END
 
-CASE_PERCENT:
-		mov rax, '%'
-		putchar
-		jmp SWITCH_END
-CASE_b:
-		mov bl, 32
-		mov rcx, 1
-		mov r9, 10000000000000000000000000000000b
-		mov rax, [rbp]
-		add rbp, 8
-		call processTwoDegree
-		jmp SWITCH_END
+		CASE_PERCENT:
+				mov rax, '%' 						;|print %
+				putChar 							;|
+				jmp SWITCH_END 						;|
 
-CASE_c:
-		call process_ascii
-		jmp  SWITCH_END
-CASE_d:
-		mov  r9, 10
-		call processNumber
-		jmp  SWITCH_END
+		CASE_b:
+				mov rcx, ONES_IN_MASK2  			;rcx = 1
+				mov r13, MASK2 						;r13 = 1b
+				getArgument 						;rax = next argument
+				call processTwoDegreeSigned
+				jmp SWITCH_END
 
-CASE_o:
-		mov bl, 33
-		mov rcx, 3
-		mov r9, 111000000000000000000000000000000b
-		mov rax, [rbp]
-		add rbp, 8
-		call processTwoDegree
-		jmp SWITCH_END
+		CASE_c:
+				getArgument							;rax = next argument
+				putChar
+				jmp  SWITCH_END
 
-CASE_s:
-		call processStr
-		jmp  SWITCH_END
+		CASE_d:
+				mov  r9, BASE10 					;r9 = 10
+				getArgument							;rax = next argument
+				call processNumberSigned
+				jmp  SWITCH_END
 
-CASE_x:
-		mov bl, 32
-		mov rcx, 4
-		mov r9, 11110000000000000000000000000000b
-		mov rax, [rbp]
-		add rbp, 8
-		call processTwoDegree
-		jmp SWITCH_END
+		CASE_o:
+				mov rcx, ONES_IN_MASK8 				;rcx = 3
+				mov r13, MASK8 						;r13 = 111b
+				getArgument							;rax = next argument
+				call processTwoDegreeSigned
+				jmp SWITCH_END
 
-process_ascii:
-		mov rax, [rbp]
-		add rbp, 8
-		putchar
-		ret
+		CASE_s:
+				call processStr
+				jmp  SWITCH_END
 
-printNumberToBuffer:
-		pop r10
+		CASE_u:
+				mov  r9, BASE10						;r9 = 10
+				getArgument							;rax = next argument
+				call processNumberUnsigned
+				jmp  SWITCH_END
+
+		CASE_x:
+				mov rcx, ONES_IN_MASK16 			;rcx = 4
+				mov r13, MASK16 					;r13 = 1111b
+				getArgument							;rax = next argument
+				call processTwoDegreeSigned
+				jmp SWITCH_END
+;----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;movNumberToBuffer:
+;		moves symbol from stack to buffer (using in printing numbers)
+;
+;Entry:
+;		r12 - count of pushed numbers
+;
+;Destr:
+;		r10
+;----------------------------------------------------------------------------------------------------------------------------------------------
+movNumberToBuffer:
+		pop r10 			;save return address
 
 	.printNumber:
-		pop rax
-		putchar
-		loop .printNumber
+		pop rax				;get symbol from stack
+		putChar 			;print sybmol
+		dec r12 			;cnt_to_print--
+		cmp r12, 0
+		jne .printNumber
 
-		push r10
+		push r10 			;push address
 		ret
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
-processNumber:
-		mov  r10, rdx
-		xor  rax, rax
-		xor  rdx, rdx
-		xor  rcx, rcx
 
-		mov qword rax, [rbp]		;|rax = mem[argumet++]
-		add rbp, 8
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;processNumber(Un)Signed:
+;		process number and pushes to stack in revers order and than call
+;		printBuffer which prints number to buffer
+;
+;Entry:
+;		rax = argument (number to print)
+;		r9  = base of number to print
+;
+;Destr:
+;		rdx, r12
+;----------------------------------------------------------------------------------------------------------------------------------------------
+processNumberSigned:
+		call checkAndPrintSign 					;check sign if it is signed call
+processNumberUnsigned:
+
+		xor  rdx, rdx 				 			;|preparing register for work
+		xor  r12, r12 							;|
 
 	nextNum:
-		div r9
- 		mov rdx, [NUM_TABLE + rdx]
-		inc rcx
-		push rdx
- 		xor rdx, rdx
-		cmp rax, 0
-		jne nextNum
+		div r9 									;|rdx = rax % 10
+ 		mov dl, [NUM_TABLE + rdx] 				;|dl  = ASCII code
+		inc r12 								;|pushed_symbols++
+		push rdx 								;|push ASCII code
+ 		xor rdx, rdx 							;|prepare rdx for next iteration
+		cmp rax, 0 								;|check if rax == 0
+		jne nextNum 							;|
 
-		mov  rdx, r10
-		call printNumberToBuffer
+		call movNumberToBuffer					;move number to buffer
 
 		ret
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;processStr:
+;		moves symbols from inputted string to buffer
+;
+;Destr:
+;		rax
+;----------------------------------------------------------------------------------------------------------------------------------------------
 processStr:
-		push rsi
-		mov  rsi, [rbp]
-		add  rbp, 8
+		push rsi 					;save rsi
+		mov  rsi, [rbp] 			;|get address argument
+		add  rbp, 8 				;|
 	copy:
-		lodsb
-		cmp al, 0
+		lodsb 						;al = [rsi]
+		cmp al, 0 					;if al == END_OF_STRING
 		je end_of_copy
 
-		putchar
+		putChar
 		jmp copy
 
 	end_of_copy:
-		pop rsi
+		pop rsi 					;recover rsi
 
 		ret
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
-;rax = number to print
-;r10 = mask
-;rbx = 32
-;cl = num of 1
 
-;example: r10 = 111 << 31
-processTwoDegree:
-	.set_mask:
-		mov r10, rax
-		and rax, r9
-		xchg rax, r10
-		cmp r10, 0
-		jne .nextNum
-		shr r9, cl
-		sub bl, cl
-		jmp .set_mask
-
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;processTwoDegree(Un)Signed:
+;		pushes in stack 2 degree number in reversed order
+;
+;Entry:
+;		r13 = mask
+;		rax = number
+;		cl  = count of ones in mask
+;
+;Destr:
+;		r10, r12
+;----------------------------------------------------------------------------------------------------------------------------------------------
+processTwoDegreeSigned:
+		push rcx 									;save rcx because syscall which called in putchar in checkSigh
+		call checkAndPrintSign
+		pop rcx 									;recover rcx
+processTwoDegreeUnsigned:
+		xor r12, r12 								;prepare reg for work
 	.nextNum:
-		mov  r10, rax
-		and  rax, r9
-		sub  rbx, rcx
+		mov  r10, rax 								;save rax
+		and  r10, r13 								;get bytes by mask
 
-		xchg rbx, rcx
-		shr  rax, cl
-		xchg rbx, rcx
-
-		mov rax, [NUM_TABLE + rax]
-		putchar
-
-		shr  r9, cl
-		xchg r10, rax
-		cmp  r9, 0
+		mov  byte r10b, [NUM_TABLE + r10]   		;get ASCII code
+		push r10 									;push tos stack ASCII
+		shr  rax, cl 								;get needed number
+		inc  r12 									;cnt_of_pushed_symbols
+		cmp  rax, 0 								;if (rax == 0) goto .endOfCalc
 		jne .nextNum
 
+	.endOfCalc:
+		call movNumberToBuffer
+
+		ret
+;----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;checkSign:
+;		check if number is negative and print '-' if it is. also convert number to positive for print
+;
+;Entry:
+;		rax = number
+;
+;Destr:
+;		r10
+;----------------------------------------------------------------------------------------------------------------------------------------------
+checkAndPrintSign:
+		mov r10, rax 				;save rax
+		and rax, SIGN_MASK 			;check sign bit
+		cmp rax, 0 					;|if bit == 1:
+		je .positive  				;|		print sign
+		mov byte al, '-' 		 	;|
+		putChar						;|
+
+		mov rax, r10 				;|set rax positive and return
+		neg eax 					;|
 		ret
 
-; checkSign:
-; 		and  rax, 10000000000000000000000000000000b
-; 		cmp  rax, 0
-; 		je .positive
-
-; 		mov r9
-
-; 	.positive:
-; 		ret
+	.positive:
+		mov rax, r10 				;recover rax
+		ret
+;----------------------------------------------------------------------------------------------------------------------------------------------
 
 section 	.data
 NUM_TABLE: db "0123456789ABCDEF"
--- testPrint.cpp
+++ testPrint.cpp
@@ -3,31 +3,12 @@ extern "C" int myPrint(const char* fmt, ...);
 
 int main()
 {
-	// int a = 52;
-	// myPrint("TEST 1");
-	// for(int i = 0; i < 100; i++)
-	// {
-	// 	myPrint("d = %d | o = %o | b = %b | c = %c | x = %x| a = %d", i, i, i, i, i, a);
-	// 	printf("\n");
-	// }
-
-	// myPrint("TEST2 (%%)");
-	// printf("\n");
-
-	// myPrint("TEST3 (OVERFLOW)");
-	// printf("\n");
-	// myPrint("123456789ABCDF123456789ABCDFAAA  %d", a);
-	// printf("\n");
-
-	// char test4[] = "test4";
-	// myPrint("%c = %s!", a, test4);
-
 	const char *format = "%d\n%b\n%c\n%s\n%%\n%x\n%c\n%o\n%o\n";
-    long long   par1 = 123456;
+    int   par1 = -123456;
     int         par2 = 5;
     const char  par3 = 'c';
     const char *par4 = "STRING";
-    long long   par5 = 0xA1B2C3DE;
+    long long   par5 = 0x01B2C3DE;
     const char  par6 = 'f';
     int         par7 = -1234;
     int         par8 = 05555;
@@ -37,8 +18,7 @@ int main()
                     par4, par5, par6, par7, par8,
                     -1, "love", 3802, 100, 33, 30);
 
-
-	// int c = myPrint("%x", 20);
+	myPrint("a = %d\n", a);
 
 	return 0;
 }
